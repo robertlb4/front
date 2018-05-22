cae6c5ec3d7920d94dd96d22f8602d79875972df
Gino17 <galentro451@gmail.com>
2018-05-22 15:54:53 -0700

    added forgot password button to login page which pushes a new empty forgot password page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { ChartsModule } from 'ng2-charts/ng2-charts';
+import { ForgotPasswordPage } from '../pages/forgot-password/forgot-password';
 
 import { Items } from '../mocks/providers/items';
 import { Settings, User, Api } from '../providers';
@@ -39,6 +40,7 @@ export function provideSettings(storage: Storage) {
 @NgModule({
   declarations: [
     MyApp,
+    ForgotPasswordPage,
   ],
   imports: [
     BrowserModule,
@@ -57,6 +59,7 @@ export function provideSettings(storage: Storage) {
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
+    ForgotPasswordPage,
   ],
   providers: [
     Api,
